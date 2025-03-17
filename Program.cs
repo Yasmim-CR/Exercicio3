1bc828dee0f5b7e283e3882ddb7d7c58de43a090
Yasmim de Castro <yas.castr0.r@gmail.com>
2025-03-17 09:32:22 -0300

    Finalizar Programa

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Preparar...");
+Thread.Sleep(1000);
+Console.WriteLine("Apontar...");
+Thread.Sleep(1000);
+Console.WriteLine("FOGO!");
